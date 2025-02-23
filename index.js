d321bbe00e6a52694141c990a305286546d41a46
arkham55 <154397924+arkham55@users.noreply.github.com>
2025-02-23 11:02:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./db');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Import routes
+const produkRoutes = require('./routes/produk');
+const userRoutes = require('./routes/user');
+
+// Menggunakan routes
+app.use('/api/produk', produkRoutes);
+app.use('/api/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
+});
 
